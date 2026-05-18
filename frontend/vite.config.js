ed0fd1f85133b620ea9ff1c085a4666bd4e5e865
Bintang Prayoga <bprayoga1927@gmail.com>
2026-05-18 14:04:29 +0700

    Migrasi tailwind dari CDN ke Package Node

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 import preact from '@preact/preset-vite';
 
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), tailwindcss()],
   server: {
     host: true, // Agar bisa diakses dari luar kontainer
     port: 3000, // PAKSA menggunakan port 3000 sesuai Nginx & Dockerfile
